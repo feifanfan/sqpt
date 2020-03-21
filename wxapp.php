f2094a6d6c6444ef294c73068e35f6d5e9cee76b
hackerff <1141524967@qq.com>
2020-03-21 16:27:17 +0800

    调查问卷

--- wxapp.php
+++ wxapp.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
     if(version_compare(PHP_VERSION,'5.3.0','<')) die('require PHP > 5.3.0');
     header("Content-type:text/html;charset=utf-8");
@@ -10,6 +10,7 @@
     if (!isset($wx_controller_arr[1])){
         $wx_controller_arr[1]='index';
     }
+    var_dump(11);
     unset($_GET['i']);
     unset($_GET['t']);
     unset($_GET['v']);
@@ -22,29 +23,4 @@
 
     define('ROOT_PATH',str_replace('\\','/',dirname(__FILE__)).'/');
     define('RUNTIME_PATH','./Runtime/');
-=======
-<?php
-    if(version_compare(PHP_VERSION,'5.3.0','<')) die('require PHP > 5.3.0');
-    header("Content-type:text/html;charset=utf-8");
-    define('APP_DEBUG',true);
-    define('BIND_MODULE','Home');
-    define('APP_PATH','./Modules/');
-    $wx_controller = $_REQUEST['controller'];
-    $wx_controller_arr = explode('.',$wx_controller);
-    if (!isset($wx_controller_arr[1])){
-        $wx_controller_arr[1]='index';
-    }
-    unset($_GET['i']);
-    unset($_GET['t']);
-    unset($_GET['v']);
-    unset($_GET['form']);
-    unset($_GET['do']);
-    $_GET['c'] = $wx_controller_arr[0];
-    $_GET['a'] = $wx_controller_arr[1];
-    unset($_GET['controller']);
-    unset($_GET['m']);
-
-    define('ROOT_PATH',str_replace('\\','/',dirname(__FILE__)).'/');
-    define('RUNTIME_PATH','./Runtime/');
->>>>>>> ef11f15919d5824947de9dcd8ef92c0c4e919b53
     require './ThinkPHP/ThinkPHP.php';
